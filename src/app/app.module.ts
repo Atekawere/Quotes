b708ab174e150d7e433972de47fd83829a8f0b60
Atekawere <jacintawere2@gmail.com>
2020-04-01 23:56:25 +0300

    added the pipes file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { QuoteComponent } from './quote/quote.component';
 import { GoalDetailComponent } from './goal-detail/goal-detail.component';
 import { QuoteDetailComponent } from './quote-detail/quote-detail.component';
+import { StrikethroughDirective } from './strikethrough.directive';
+import { DateCountPipe } from './date-count.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     QuoteComponent,
     GoalDetailComponent,
-    QuoteDetailComponent
+    QuoteDetailComponent,
+    StrikethroughDirective,
+    DateCountPipe
   ],
   imports: [
     BrowserModule,
